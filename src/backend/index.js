5a9032fc9352dab042254186e943dd45572f1eba
Isaias S. Silva <luiz.isaias95@gmail.com>
2018-05-27 14:48:08 -0300

    Only active for video pages

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,1 +1,20 @@
 console.log('background !')
+
+/*
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.storage.sync.set({color: '#3aa757'}, function () {
+    console.log('The color is green.')
+  })
+}) */
+
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [new chrome.declarativeContent.PageStateMatcher({
+        pageUrl: {hostEquals: 'www.youtube.com', urlContains: '?v'}
+      })
+      ],
+      actions: [new chrome.declarativeContent.ShowPageAction()]
+    }])
+  })
+})
