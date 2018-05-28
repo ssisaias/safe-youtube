ab1c6e0574939b61779ca9c44a2d0fcdb5f3724d
Isaias S. Silva <luiz.isaias95@gmail.com>
2018-05-28 09:10:20 -0300

    sends the current video URL to service

--- src/popup/root.vue
+++ src/popup/root.vue
@@ -8,13 +8,15 @@
         </el-header>
         <el-main>
           div(v-if="loading" v-loading="loading" element-loading-text="Carregando..." style="margin-top: 25px")
-          div
-            span {{message}}
+          div(v-if="!error_message")
+            span.mensagem {{message}}
             i(class="el-icon-success" style="margin: 10px 0 0 0")
               span(style="margin: 0 0 0 10px") {{good_value}}
             hr
             i(class="el-icon-warning") 
               span(style="margin: 0 0 0 10px") {{bad_value}}
+          div(v-if="error_message")
+            h3(style="color:#c20707") Error: {{error_message}}
         </el-main>
       </el-container>
       
@@ -33,17 +35,37 @@
     computed: { },
     created () {},
     mounted () {
-      axios.get(`http://127.0.0.1:5000/`).then(response => {
-        // JSON responses are automatically parsed.
-        this.value = response.data
-        console.log(this.value)
-        this.good_value = this.value.good
-        this.bad_value = this.value.bad
-        this.loading = false
-      })
-        .catch(e => {
-          this.value = e
+      let vm = this // Inside callback functions 'this' is for the function context, so we got to get the vue instance here
+      chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+        var url = new URL(tabs[0].url)
+        let videoId = url.search.toString().split('=')[1]
+        console.log(videoId)
+        axios.get(`http://127.0.0.1:5000/video?id=` + videoId).then(response => {
+          // JSON responses are automatically parsed.
+          if (response.status === 200) {
+            vm.value = response.data
+            console.log(vm.value)
+            vm.good_value = vm.value.good
+            vm.bad_value = vm.value.bad
+            vm.loading = false
+            if (!vm.good_value || !vm.bad_value) {
+              vm.error_message = 'Problemas ao processar dados do servidor!'
+            }
+          } else if (response.status === 202) {
+            vm.loading = false
+            vm.error_message = 'Este vídeo não está disponível, vamos tentar classificá-lo! Tente novamente em alguns minutos!'
+          } else {
+            vm.loading = false
+            vm.error_message = 'Problemas ao processar dados do servidor! ' + response.status
+            console.log(response)
+          }
         })
+          .catch(e => {
+            console.log(e)
+            this.error_message = e.message
+            this.loading = false
+          })
+      })
     },
     methods: {
       tab () {
@@ -59,7 +81,9 @@
     margin: 0;
     overflow:hidden;
   }
-  
+  span .mensagem{
+    color: pink;
+  }
   .el-header{
     background-color: #B3C0D1;
     color: #333;
